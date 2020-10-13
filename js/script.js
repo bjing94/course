c93a28f6dd074a3a45f4d4d33bc66d955c23c704
bjing <kovriklol@gmail.com>
2020-10-13 10:24:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+
+
+const category='gachibass';
+console.log(`https://someurl.net/${category}/5`);
+
+let incr=10;
+let decr=10;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(65*1 =='65');
