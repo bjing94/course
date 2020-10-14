4e4921035e62f1df2a4a8d27240cdba3776feb7a
bjing <kovriklol@gmail.com>
2020-10-14 15:09:33 +0300

    started jebroni

--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@
 
 
 
-
-
-const category='gachibass';
-console.log(`https://someurl.net/${category}/5`);
-
-let incr=10;
-let decr=10;
-
-console.log(++incr);
-console.log(decr--);
-
-console.log(65*1 =='65');
+//only for me!
+let stopButton=document.getElementById("stopButton");
+let playButton=document.getElementById("playButton");
+playButton.addEventListener("mousedown",function()
+{
+   playButton.style.opacity="0";
+   playButton.style.zIndex="0";
+   stopButton.style.opacity="1.0";
+   stopButton.style.zIndex="1";
+}
+);
+if(!stopButton)
+{
+    console.log("oh fuck!");
+}
+stopButton.addEventListener("mousedown",function()
+{
+    stopButton.style.opacity="0";
+    stopButton.style.zIndex="0";
+    playButton.style.opacity="1.0";
+    playButton.style.zIndex="1";
+}
+);
