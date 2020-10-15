8c536c83d91a14a4ba1a9b99961cd9adbf193ffb
bjing <kovriklol@gmail.com>
2020-10-15 12:08:34 +0300

    added music

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 "use strict";
 
 
+//courses
+
+
 
 //only for me!
-let stopButton=document.getElementById("stopButton");
-let playButton=document.getElementById("playButton");
-playButton.addEventListener("mousedown",function()
-{
-   playButton.style.opacity="0";
-   playButton.style.zIndex="0";
-   stopButton.style.opacity="1.0";
-   stopButton.style.zIndex="1";
-}
-);
-if(!stopButton)
-{
+let mainSong = document.getElementById("mainSong");
+let stopButton = document.getElementById("stopButton");
+let playButton = document.getElementById("playButton");
+playButton.addEventListener("mousedown", function () {
+    playButton.style.opacity = "0";
+    playButton.style.zIndex = "0";
+    stopButton.style.opacity = "1.0";
+    stopButton.style.zIndex = "1";
+    mainSong.play();
+});
+if (!stopButton) {
     console.log("oh fuck!");
 }
-stopButton.addEventListener("mousedown",function()
-{
-    stopButton.style.opacity="0";
-    stopButton.style.zIndex="0";
-    playButton.style.opacity="1.0";
-    playButton.style.zIndex="1";
-}
-);
+stopButton.addEventListener("mousedown", function () {
+    stopButton.style.opacity = "0";
+    stopButton.style.zIndex = "0";
+    playButton.style.opacity = "1.0";
+    playButton.style.zIndex = "1";
+    mainSong.pause();
+});
